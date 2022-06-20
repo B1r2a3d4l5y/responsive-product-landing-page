332f11330b982c61ba542f1b7d5da1f1c2b1017d
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2022-06-20 14:06:20 +0200

    added js folder with a script.js file with code

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#reviewBtn1") .click(function(){
+        $("#review1").show().animate({opacity:1});
+
+    });
+    $("#reviewBtn2").click(function(){
+        $("#review2").show().animate({opacity:1});
+    });
+    $("#reviewBtn3").click(function(){
+        $("#review3").show().animate({opacity:1});
+    });
+})
